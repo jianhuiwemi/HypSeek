ce92dd9824c4c0e048e4b6c77865437d4be701a2
Jianhui Wang <151813614+jianhuiwemi@users.noreply.github.com>
2025-11-24 21:31:07 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,41 +1,44 @@
 #!/usr/bin/env bash
 
+mode=$1
+if [ "$mode" == "FEP" ]; then
+    valid_set="FEP"
+    disable_valid="--disable-validation"
+    metric_args=""     # no best-checkpoint-metric
+elif [ "$mode" == "CASF" ]; then
+    valid_set="CASF"
+    disable_valid=""
+    metric_args="--best-checkpoint-metric valid_bedroc --maximize-best-checkpoint-metric"
+else
+    echo "Usage: bash train.sh [FEP | CASF]"
+    exit 1
+fi
+
+
 data_path="./data"
 
-save_root="./save_hyper_hdr"
+save_root="/save_root"
 save_name="screen_pocket_hyperbolic"
 save_dir="${save_root}/${save_name}/savedir_screen"
 tmp_save_dir="${save_root}/${save_name}/tmp_save_dir_screen"
 tsb_dir="${save_root}/${save_name}/tsb_dir_screen"
-mkdir -p "${save_dir}"
-mkdir -p "${tmp_save_dir}"
-mkdir -p "${tsb_dir}"
-mkdir -p "${save_root}/train_log"
+mkdir -p "${save_dir}" "${tmp_save_dir}" "${tsb_dir}" "${save_root}/train_log"
 
 n_gpu=4
 MASTER_PORT=10068
 
-finetune_mol_model="./pretrain/mol_pre_no_h_220816.pt"     
-finetune_pocket_model="./pretrain/pocket_pre_220816.pt"   
+finetune_mol_model="./pretrain/mol_pre_no_h_220816.pt"
+finetune_pocket_model="./pretrain/pocket_pre_220816.pt"
 
 batch_size=24
+batch_size_valid=32
 epoch=50
 warmup=0.06
 update_freq=1
-dist_threshold=8.0
-recycling=3
 lr=1e-4
-
-
-half_aperture_K=0.1    
-aperture_eta=1.2       
-alpha_rank=1.0         
-alpha_corr=0.0          
-gamma_ce=1.0            
 
 export NCCL_ASYNC_ERROR_HANDLING=1
 export OMP_NUM_THREADS=1
-
 UNICORE_TRAIN=$(which unicore-train)
 
 CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun \
@@ -43,12 +46,14 @@ CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun \
   --master_port=${MASTER_PORT} \
   ${UNICORE_TRAIN} ${data_path} \
     --user-dir ./unimol \
+    --task train_task \
+    --arch three_hybrid_model \
+    --loss three_hybrid_loss \
     --train-subset train \
+    --valid-subset valid \
+    --valid-set ${valid_set} \
     --num-workers 8 \
     --ddp-backend c10d \
-    --task train_task \
-    --loss three_hybrid_loss \
-    --arch three_hybrid_model \
     --max-pocket-atoms 256 \
     --optimizer adam \
     --adam-betas "(0.9, 0.999)" \
@@ -59,6 +64,7 @@ CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun \
     --warmup-ratio ${warmup} \
     --max-epoch ${epoch} \
     --batch-size ${batch_size} \
+    --batch-size-valid ${batch_size_valid} \
     --fp16 \
     --fp16-init-scale 4 \
     --fp16-scale-window 256 \
@@ -67,7 +73,7 @@ CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun \
     --tensorboard-logdir ${tsb_dir} \
     --log-interval 100 \
     --log-format simple \
-    --disable-validation \
+    --validate-interval 1 \
     --all-gather-list-size 2048000 \
     --save-dir ${save_dir} \
     --tmp-save-dir ${tmp_save_dir} \
@@ -79,9 +85,6 @@ CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun \
     --max-lignum 16 \
     --learn-curv \
     --protein-similarity-thres 1.0 \
-    --half-aperture-K ${half_aperture_K} \
-    --aperture-eta ${aperture_eta} \
-    --alpha-rank ${alpha_rank} \
-    --alpha-corr ${alpha_corr} \
-    --gamma-ce ${gamma_ce} \
-
+    ${disable_valid} \
+    ${metric_args} \
+  > ${save_root}/train_log/train_log_${save_name}.txt 2>&1
